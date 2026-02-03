b9cb2ccd76d4a47697a1bfe6c26d0cef1202fb36
Antton Ducos <antton.ducos@epitech.eu>
2026-02-03 20:59:58 +0100

    hotfix: add mkdir permission

--- reset.sh
+++ reset.sh
@@ -1,9 +1,14 @@
-#!/bin/bash                                                                     
+#!/bin/bash
 echo "Suppression des containers et volumes..."
 docker compose down -v
 
 echo "Nettoyage des uploads..."
 rm -rf ./init-API/uploads/*
+
+echo "Creation du dossier uploads avec les bonnes permissions..."
+mkdir -p ./init-API/uploads/photos
+sudo chown -R 1001:1001 ./init-API/uploads
+chmod -R 755 ./init-API/uploads
 
 echo "Rebuild..."
 docker compose up --build
